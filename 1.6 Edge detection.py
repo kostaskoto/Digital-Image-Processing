f004ac2ced25235a312017432bcc626ef8449462
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-04-25 17:58:11 +0300

    1.6.1 done

--- 1.6 Edge detection.py
+++ 1.6 Edge detection.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import sys
+from skimage import color 
+import math
 
 img = np.asarray(Image.open('clock.jpg'))
-print(repr(img))
-imgplot = plt.imshow(img)
+imgGray = color.rgb2gray(img)
+print(imgGray)
+
+imgplot = plt.imshow(imgGray, cmap='gray')
+plt.show()
+
+
+
+for i in range(len(imgGray)-2):
+    for j in range(len(imgGray[0])-2):
+        Gx=((2*imgGray[i+2][j+1]+imgGray[i+2][j]+imgGray[i+2][j+2])-(2*imgGray[i][j+1]+imgGray[i][j]+imgGray[i][j+2]))
+
+        Gy=((2*imgGray[i+1][j+2]+imgGray[i][j+2]+imgGray[i+2][j+2])-(2*imgGray[i+1][j]+imgGray[i][j]+imgGray[i+2][j]))
+        imgGray[i][j]=math.sqrt(Gx**2+Gy**2)
+
+imgplot = plt.imshow(imgGray, cmap='gray')
 plt.show()
