bd0f83967153fd76d32300f3926570ca06c089ac
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-04-25 20:49:54 +0300

    Changed layout 1.6.1

--- 1.6 Edge detection.py
+++ 1.6 Edge detection.py
@@ -9,11 +9,10 @@ img = np.asarray(Image.open('clock.jpg'))
 imgGray = color.rgb2gray(img)
 print(imgGray)
 
-imgplot = plt.imshow(imgGray, cmap='gray')
-plt.show()
+f, imgplot = plt.subplots(1,2)
+imgplot[0].imshow(imgGray, cmap='gray')
 
-
-
+#ex 1
 for i in range(len(imgGray)-2):
     for j in range(len(imgGray[0])-2):
         Gx=((2*imgGray[i+2][j+1]+imgGray[i+2][j]+imgGray[i+2][j+2])-(2*imgGray[i][j+1]+imgGray[i][j]+imgGray[i][j+2]))
@@ -21,5 +20,8 @@ for i in range(len(imgGray)-2):
         Gy=((2*imgGray[i+1][j+2]+imgGray[i][j+2]+imgGray[i+2][j+2])-(2*imgGray[i+1][j]+imgGray[i][j]+imgGray[i+2][j]))
         imgGray[i][j]=math.sqrt(Gx**2+Gy**2)
 
-imgplot = plt.imshow(imgGray, cmap='gray')
+imgplot[1].imshow(imgGray, cmap='gray')
+
+#ex2
+
 plt.show()
