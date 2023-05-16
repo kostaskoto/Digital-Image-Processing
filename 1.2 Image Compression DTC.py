2d6634f37c158848f54e0f23b9da603610355660
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-05-16 15:10:10 +0300

    zonal mask done

--- 1.2 Image Compression DTC.py
+++ 1.2 Image Compression DTC.py
@@ -13,9 +13,7 @@ def twoDDCT(img, blockSize):
     for i in range(0, len(img), blockSize):
         for j in range(0, len(img[0]), blockSize):
             dctImg[i:i+blockSize, j:j+blockSize] = cv2.dct(img[i:i+blockSize, j:j+blockSize])
-            print(dctImg[i:i+blockSize, j:j+blockSize])
-    
-
+             
     return dctImg
 
 def inverseTwoDDCT(dctImg, blockSize):
@@ -28,16 +26,44 @@ def inverseTwoDDCT(dctImg, blockSize):
             img[i:i+blockSize, j:j+blockSize] = cv2.idct(dctImg[i:i+blockSize, j:j+blockSize])
     return img
 
+def zonalMask(blockSize, maskSize):
+    # blockSize is the size of the block
+    # maskSize is the size of the mask
+    # return the zonal mask
+    mask = np.zeros((blockSize, blockSize))
+    for i in range(maskSize):
+        for j in range(maskSize):
+            mask[i][j] = 1
+    return mask
+
+def thresholdingMask(blockSize, threshold):
+    # blockSize is the size of the block
+    # threshold is the threshold value
+    # return the thresholding mask
+    mask = np.zeros((blockSize, blockSize))
+    for i in range(blockSize):
+        for j in range(blockSize):
+            if i + j < threshold:
+                mask[i][j] = 1
+    return mask
+
 img = cv2.imread("lenna.jpg", cv2.IMREAD_GRAYSCALE)
 print(repr(img))
-f, imgplot = plt.subplots(2,3)
+f, imgplot = plt.subplots(3,2)
 imgplot[0][0].imshow(img, cmap='gray')
 
-
-dctImg = twoDDCT(np.float32(img) / 255.0, 32)
+blockSize = 32
+dctImg = twoDDCT(np.float32(img) / 255.0, blockSize)
 imgplot[0][1].imshow(dctImg, cmap='gray')
 
-imgplot[0][2].imshow(inverseTwoDDCT(dctImg, 32), cmap='gray')
+zonalDCT = np.zeros_like(dctImg)
+for i in range(0, len(dctImg), blockSize):
+        for j in range(0, len(dctImg[0]), blockSize):
+            zonalDCT[i:i+blockSize, j:j+blockSize] = np.multiply(dctImg[i:i+blockSize, j:j+blockSize], zonalMask(blockSize, 8))
+    
+
+imgplot[1][0].imshow(zonalDCT, cmap='gray')
+imgplot[1][1].imshow(inverseTwoDDCT(zonalDCT, blockSize), cmap='gray')
 
 plt.show()
 
