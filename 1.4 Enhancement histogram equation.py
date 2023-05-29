fc8acc8dac2faa373621184957feda6444eb7824
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-05-29 20:51:33 +0300

    finnished 1.4

--- 1.4 Enhancement histogram equation.py
+++ 1.4 Enhancement histogram equation.py
@@ -32,8 +32,8 @@ def localHistogramEqualization(img, blockSize):
     # blockSize is the size of the block
     # return the local histogram equalized image
     imgEq = np.zeros_like(img)
-    for i in range(0, len(img), blockSize):
-        for j in range(0, len(img[0]), blockSize):
+    for i in range(len(img) - blockSize):
+        for j in range(len(img[0]) - blockSize):
             block = img[i:i+blockSize, j:j+blockSize]
             blockEq = histogramEqualization(block)
             imgEq[i:i+blockSize, j:j+blockSize] = blockEq
@@ -46,9 +46,9 @@ img3 = np.asarray(Image.open('dark_road_3.jpg'))
 
 f, imgplot = plt.subplots(6,3)
 
+imgplot[0][0].imshow(img1, cmap='gray')
 imgplot[0][1].imshow(img2, cmap='gray')
 imgplot[0][2].imshow(img3, cmap='gray')
-imgplot[0][0].imshow(img1, cmap='gray')
 
 h1 = histogram(img1)
 h2 = histogram(img2)
@@ -62,9 +62,9 @@ imgEq1 = histogramEqualization(img1)
 imgEq2 = histogramEqualization(img2)
 imgEq3 = histogramEqualization(img3)
 
-imgplot[2][1].imshow(imgEq1, cmap='gray')
-imgplot[2][2].imshow(imgEq2, cmap='gray')
-imgplot[2][0].imshow(imgEq3, cmap='gray')
+imgplot[2][0].imshow(imgEq1, cmap='gray')
+imgplot[2][1].imshow(imgEq2, cmap='gray')
+imgplot[2][2].imshow(imgEq3, cmap='gray')
 
 h1Eq = histogram(imgEq1)
 h2Eq = histogram(imgEq2)
@@ -76,13 +76,13 @@ imgplot[3][2].plot(h3Eq)
 
 blockSize = 4
 
-imgLocEq1 = localHistogramEqualization(img1, blockSize)
-imgLocEq2 = localHistogramEqualization(img2, blockSize)
-imgLocEq3 = localHistogramEqualization(img3, blockSize)
+imgLocEq1 = localHistogramEqualization(img1, 16)
+imgLocEq2 = localHistogramEqualization(img2, 50)
+imgLocEq3 = localHistogramEqualization(img3, 100)
 
-imgplot[4][1].imshow(imgLocEq1, cmap='gray')
-imgplot[4][2].imshow(imgLocEq2, cmap='gray')
-imgplot[4][0].imshow(imgLocEq3, cmap='gray')
+imgplot[4][0].imshow(imgLocEq1, cmap='gray')
+imgplot[4][1].imshow(imgLocEq2, cmap='gray')
+imgplot[4][2].imshow(imgLocEq3, cmap='gray')
 
 h1LocEq = histogram(imgLocEq1)
 h2LocEq = histogram(imgLocEq2)
